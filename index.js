a9495c06cb1f98707aaeb34ab54a137c451b6b20
SarvinMulani <140257574+SarvinMulani@users.noreply.github.com>
2025-03-15 23:19:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const inputBox = document.querySelector('.js-input');
+const list_container = document.querySelector('.listContainer');
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert('You must enter task before adding!');
+    } else {
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        list_container.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    saveData();
+}
+
+list_container.addEventListener('click', function(e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+        saveData();
+    } else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem('data', list_container.innerHTML);
+}
+
+function showTask() {
+    list_container.innerHTML = localStorage.getItem('data');
+}
